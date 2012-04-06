bdbfa5d030d4dbefafc679448749ea14b61f0416
lmwang <lmwang@gmail.com>
2012-04-06 09:59:40 -0700

    data structure for choice model WIP

--- choice_model.py
+++ choice_model.py
@@ -10,16 +10,11 @@ from pandas import DataFrame, MultiIndex
 from itertools import product
 import numpy as np
 
-
 n = 5
 nalts = 3
 
-#levels = [xrange(1, n+1), xrange(1, nalts+1)]
 ids=zip(*product(xrange(1, n+1), xrange(1, nalts+1)))
 agent_id, choice_id = ids
-#midx = MultiIndex(levels=levels, labels=labels,
-#                  names=['agent_id', 'choice_id'])
-
 sdata = SparseDataFrame({'agent_id' : agent_id,
                          'choice_id': choice_id},
                         default_fill_value=0.0)
@@ -27,10 +22,6 @@ sdata = SparseDataFrame({'agent_id' : agent_id,
 #alt specific variable
 int_var1 = np.zeros(n*nalts)
 int_var1[1:n*nalts:nalts] = np.random.randn(1)
-
-#sdata = SparseDataFrame({'int_var1' : int_var1}, 
-#                        index=midx, default_fill_value=0.0)
-
 sdata['int_var1'] = int_var1
 
 #alt specific dummy
@@ -39,17 +30,24 @@ int_var2[2:n*nalts:nalts] = 1
 sdata['int_var2'] = int_var2
 
 #interaction variable
-int_var3 = np.random.randn(n*nalts)
-sdata['int_var3'] = int_var3
+int_var3 = np.random.randn(n*nalts).reshape((n, nalts))
+sdata['int_var3'] = int_var3.ravel()
 
+#set index
 sdata = sdata.set_index(['agent_id', 'choice_id'])
 
+#coefficients
 beta = DataFrame({'int_var1': [.5],
                   'int_var2': [-2.0],
                   'int_var3': [-0.5]
                  })
+#utility
 sdata['utility'] = np.sum([sdata[v] * beta[v].ix[0] for v in beta.columns])
-sdata.join( sdata.groupby(level='agent_id')['utility'], on='agent_id', rsuffix=".sum")
+sdata['exp.utility'] = np.exp(sdata['utility']
+
+#calculate probability ##TODO
+sdata.join( sdata.groupby(level='agent_id')['exp.utility'], on='agent_id', lsuffix="sum.")
+sdata['prob'] = sdata['exp.utility'] / sdata['sum.exp.utility']
 
 if data is None:
     data = SparseDataFrame(data=int_var,
